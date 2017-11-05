af8c7f72f59760b9806617f98e7564be30aa5d75
Joshua Kayani <jkayani@ncsu.edu>
2017-11-05 14:48:25 -0500

    Wrote Cucumber feature and StepDefs for #12
    Even though the frontend hasn't been tested with the API yet, the UI
    flow is testable and appears to be working as intended. Will fix once
    the API is released.

---
+++ iTrust2/src/test/java/edu/ncsu/csc/itrust2/cucumber/DocumentPrescriptionStepDefs.java
@@ -0,0 +1,125 @@
+package edu.ncsu.csc.itrust2.cucumber;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Steps for documenting a prescription at /hcp/documentPrescription
+ *
+ * @author Joshua Kayani (jkayani)
+ *
+ */
+public class DocumentPrescriptionStepDefs {
+
+    private final WebDriver driver  = new HtmlUnitDriver( true );
+    private final String    baseUrl = "http://localhost:8080/iTrust2";
+    private final long      timeout = 1500;
+
+    /**
+     * Log in as HCP
+     *
+     * @throws InterruptedException
+     */
+    @Given ( "An HCP is authenticated" )
+    public void authenticateHCP () throws InterruptedException {
+        driver.get( baseUrl );
+        final WebElement username = driver.findElement( By.name( "username" ) );
+        username.clear();
+        username.sendKeys( "hcp" );
+        final WebElement password = driver.findElement( By.name( "password" ) );
+        password.clear();
+        password.sendKeys( "123456" );
+        final WebElement submit = driver.findElement( By.className( "btn" ) );
+        submit.click();
+    }
+
+    @Given ( "The HCP is at the document prescription page" )
+    public void loadDocumentPrescriptionPage () throws InterruptedException {
+        driver.get( baseUrl + "/hcp/documentPrescription.html" );
+        Thread.sleep( timeout );
+        System.out.println( "\n\n\n" + driver.getTitle() + "\n\n\n" );
+        Assert.assertTrue( driver.getPageSource().contains( "Document a new Prescription" ) );
+    }
+
+    @When ( "I select a patient (.+) by username" )
+    public void selectPatient ( String patient ) throws InterruptedException {
+        Thread.sleep( timeout );
+        try {
+            driver.findElement( By.cssSelector( "input[value='" + patient + "']" ) ).click();
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+
+    @When ( "I select a drug (.+) by NDC code" )
+    public void selectDrug ( String drug ) throws InterruptedException {
+        Thread.sleep( timeout );
+        try {
+            driver.findElement( By.cssSelector( "input[value='" + drug + "']" ) ).click();
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+
+    @When ( "I input start date (.+), end date (.+), dosage (.+), and renewals (.+)" )
+    public void inputPrescriptionData ( String sDate, String eDate, String dosage, String renewals )
+            throws InterruptedException {
+        try {
+            driver.findElement( By.id( "startDate" ) ).clear();
+            driver.findElement( By.id( "startDate" ) ).sendKeys( sDate );
+
+            driver.findElement( By.id( "endDate" ) ).clear();
+            driver.findElement( By.id( "endDate" ) ).sendKeys( eDate );
+
+            driver.findElement( By.id( "dosage" ) ).clear();
+            driver.findElement( By.id( "dosage" ) ).sendKeys( dosage );
+
+            driver.findElement( By.id( "numRenewals" ) ).clear();
+            driver.findElement( By.id( "numRenewals" ) ).sendKeys( renewals );
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+
+    @When ( "I submit the prescription" )
+    public void submitPrescription () {
+        try {
+            driver.findElement( By.name( "submit" ) ).click();
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+
+    @Then ( "The prescription was submitted successfully" )
+    public void seeSuccess () {
+        try {
+            Assert.assertEquals( "Valid data given!", driver.findElement( By.name( "success" ) ).getText() );
+            Assert.assertEquals( "", driver.findElement( By.name( "errorMsg" ) ).getText() );
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+
+    @Then ( "The prescription was NOT submitted successfully" )
+    public void seeFailure () {
+        try {
+            Assert.assertEquals( "An error occurred", driver.findElement( By.name( "success" ) ).getText() );
+            Assert.assertNotEquals( "", driver.findElement( By.name( "errorMsg" ) ).getText() );
+        }
+        catch ( final Exception e ) {
+            Assert.fail( e.getMessage() );
+        }
+    }
+}
---
+++ iTrust2/src/test/resources/edu/ncsu/csc/itrust/cucumber/DocumentPrescription.feature
@@ -0,0 +1,58 @@
+# Author Joshua Kayani (jkayani)
+
+Feature: Document a prescription that may/may not be associated with an office visit
+		 As an HCP
+		 I want to be able to prescribe medication to patients with/without an office visit
+		 Because that's what big pharma pays me to do
+		 
+		 
+Scenario Outline: Successfully document a prescription 
+Given An HCP is authenticated
+And   The HCP is at the document prescription page
+When  I select a patient <patient> by username
+And I select a drug <drug> by NDC code
+And I input start date <sdate>, end date <edate>, dosage <dosage>, and renewals <renewals>
+And I submit the prescription
+Then The prescription was submitted successfully
+
+Examples:
+	|patient|drug|sdate|edate|dosage|renewals|
+	|AliceThirteen|0832-0086|09/13/2017|09/14/2017|3|3|
+	|BobTheFourYearOld|0832-0087|09/13/2017|09/14/2017|3|3|
+	|TimTheOneYearOld|0832-0086|09/13/2016|09/14/2016|3|3|
+
+
+Scenario Outline: Unsuccessfully document a prescription 
+Given An HCP is authenticated
+And   The HCP is at the document prescription page
+When  I select a patient <patient> by username
+And I select a drug <drug> by NDC code
+And I input start date "<sdate>", end date "<edate>", dosage "<dosage>", and renewals "<renewals>"
+And I submit the prescription
+Then The prescription was NOT submitted successfully
+
+Examples:
+	|patient|drug|sdate|edate|dosage|renewals|
+	|AliceThirteen|0832-0087|words|more text!&^*&^*&^|3|3|
+	|BobTheFourYearOld|0832-0087|09/1/2017|09/14/2017|3|3|
+	|TimTheOneYearOld|0832-0086|09/1/2016|09/14/2016|3|3|
+	|TimTheOneYearOld|0832-0086|09/13/2016|09/1/2016|3|3|
+	|TimTheOneYearOld|0832-0086|09/13/2016|09/14/2016|0|3|
+	|TimTheOneYearOld|0832-0086|09/13/2016|09/14/2016|3|string|
+	|TimTheOneYearOld|0832-0086|09/13/2016|09/14/2016|string|string|
+	|AliceThirteen|0832-0086| | | | |
+	
+
+Scenario: Don't select a patient
+Given An HCP is authenticated
+And   The HCP is at the document prescription page
+When I submit the prescription
+Then The prescription was NOT submitted successfully
+
+Scenario: Don't select a medication
+Given An HCP is authenticated
+And   The HCP is at the document prescription page
+When  I select a patient AliceThirteen by username
+And I submit the prescription
+Then The prescription was NOT submitted successfully
+
