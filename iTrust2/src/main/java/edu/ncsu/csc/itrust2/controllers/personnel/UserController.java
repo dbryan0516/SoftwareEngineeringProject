77abfe4460c585c0ea3558d82e476fe02c49096b
Galen Abell <gjabell@ncsu.edu>
2017-11-15 10:14:33 -0500

    Removed unnecessary admin nav, added changePassword for Patient.

--- iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/personnel/UserController.java
+++ iTrust2/src/main/java/edu/ncsu/csc/itrust2/controllers/personnel/UserController.java
@@ -83,7 +83,6 @@ public class UserController {
     }
 
     @GetMapping ( "/personnel/changePassword" )
-    @PreAuthorize( "hasRole('ROLE_HCP') or hasRole('ROLE_ADMIN')" )
     public String changePassword () {
         return "/personnel/changePassword";
     }
